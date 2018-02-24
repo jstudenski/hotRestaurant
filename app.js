c4fa8d5d5e9be8f25b5a6fbb83feccbd9bb2d7b4
Dani Carter <carte470@d.umn.edu>
2018-02-24 11:56:24 -0600

    added body-parser for expresst o handle

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var mysql = require("mysql");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 var reservations = [
   {
     customerName: "John Doe",
@@ -50,38 +54,8 @@ app.post("/api/new", function(req, res) {
   res.json(newReservation);
 });
 
-
-
 // Listener
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
-
-
-
-
-
-
-
-
-// $.get("/api/table", function(data) {
-//   console.log(data);
-//   if (data) {
-
-//     for (var i = 0; i < data.length; i++) {
-//         // for each reservation do something
-//       if (i < 5) {
-//         // append to tables
-//       } else {
-//         // append to waitlist
-//       }
-
-//     }
-
-//   } else {
-//     // no reservations
-//   }
-// });
-
 
